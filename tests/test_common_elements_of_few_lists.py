a2ee6da7fb4d822a7f4b4dac99302203577b7534
Redzyk <rodzynq@gmail.com>
2019-10-29 23:33:50 +0100

    extracting common elements

--- tests/test_common_elements_of_few_lists.py
+++ tests/test_common_elements_of_few_lists.py
@@ -2,7 +2,7 @@ from calc.common_elements_of_few_lists import common_elements
 
 def test_common_elements():
 
-    result = common_elements("""1,2,3,3,7,11,11,11,2,5,7,11,13,
-                15,20,1,2,3,7,11,55,116,1,1,1,2,3,4,4,7,8,11""")
+    result = common_elements("""1,2,3,3,7,11,11,11","2,5,7,11,13,
+                15,20","1,2,3,7,11,55,116","1,1,1,2,3,4,4,7,8,11""")
 
     assert result == [2,7,11]
