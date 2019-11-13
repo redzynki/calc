e064294a307ffbe8273950467b9beadcc45abf3a
Redzyk <rodzynq@gmail.com>
2019-11-13 18:28:50 +0100

    extracting common elements from few lists - improved

--- calc/common_elements_of_few_lists.py
+++ calc/common_elements_of_few_lists.py
@@ -1,12 +1,8 @@
 # This method returns unduplicated,sorted common elements of few lists
+
 def common_elements(x):
-
-    a = [1,2,3,3,7,11,11,11]
-    b = [2,5,7,11,13,15,20]
-    c = [1,2,3,7,11,55,116]
-    d = [1,1,1,2,3,4,4,7,8,11]
-
-    x = sorted(list(set(a) & set(b) & set(c) & set(d)))
-
-    return x
+    result = set(x[0])
+    for lst in x:
+        result = result & set(lst)
+    return sorted(list(result))
 
--- tests/test_common_elements_of_few_lists.py
+++ tests/test_common_elements_of_few_lists.py
@@ -1,8 +1,20 @@
 from calc.common_elements_of_few_lists import common_elements
 
-def test_common_elements():
+def test_common_elements_1():
 
-    result = common_elements("""1,2,3,3,7,11,11,11","2,5,7,11,13,
-                15,20","1,2,3,7,11,55,116","1,1,1,2,3,4,4,7,8,11""")
-
+    result = common_elements([[1,2,3,3,7,11,1,11],
+                              [2,5,7,11,13,15,20],
+                              [1,1,2,3,4,4,7,8,11]])
     assert result == [2,7,11]
+
+def test_common_elements_2():
+    result = common_elements([[1], [2]])
+    assert result == []
+
+def test_common_elements_3():
+    result = common_elements([[1,2,3], [1,3], [1,4]])
+    assert result == [1]
+
+def test_common_elements_4():
+    result = common_elements([[2,2,5,7,8], [2,5,8],[2,5]])
+    assert result == [2,5]
